9045eaddc4c58a12bb9d58b2e1f84cd47f5d14cd
Manny1995 <immanuelda@gmail.com>
2014-12-26 16:28:31 -0800

    Added all elements

---
+++ Scripts/scrollingScript.js
@@ -0,0 +1,8 @@
+var userHasScrolled = false;
+window.onscroll = function(e) 
+{
+    userHasScrolled = true;
+    if (window.pageYOffset > 1000) {
+    //window.getElementsByClassName('body').style.backgroundImage="url(profile.jpg)"; // specify the image path here
+    }
+}
